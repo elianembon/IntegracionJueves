4e3bc31cfea7bd918a2291d3092aa44840611b58
ManuCC15 <109453485+ManuCC15@users.noreply.github.com>
2025-12-11 02:04:45 -0300

    Add Wwise to project

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,111 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_MUSICA_JUEGO = 612016811U;
+        static const AkUniqueID PLAY_PLAYER_FOOTSTEPS = 98439365U;
+        static const AkUniqueID PLAY_RANDOMS_EFFECTS = 3152444813U;
+        static const AkUniqueID PLAY_SPACE_SHIP_AMBIENT = 3386595448U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUSIC_STATE
+        {
+            static const AkUniqueID GROUP = 3826569560U;
+
+            namespace STATE
+            {
+                static const AkUniqueID EXPLORANDO = 2877940427U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID PARKOUR_FIN = 3950417851U;
+                static const AkUniqueID PARKOUR_INIT = 578826922U;
+                static const AkUniqueID ZOOLOGICO = 1797330646U;
+            } // namespace STATE
+        } // namespace MUSIC_STATE
+
+        namespace REVERBS
+        {
+            static const AkUniqueID GROUP = 3545700988U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID REVEROFF = 421874572U;
+                static const AkUniqueID REVERON = 3596033702U;
+            } // namespace STATE
+        } // namespace REVERBS
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace TYPES_FLOOR_STEPS
+        {
+            static const AkUniqueID GROUP = 4123168723U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID METAL = 2473969246U;
+                static const AkUniqueID SNOW = 787898836U;
+            } // namespace SWITCH
+        } // namespace TYPES_FLOOR_STEPS
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID PLAYBACK_RATE = 1524500807U;
+        static const AkUniqueID PLAYER_SPEED = 1062779386U;
+        static const AkUniqueID RPM = 796049864U;
+        static const AkUniqueID SPACE_SHIP_GRAVE = 106580904U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SPACESHIP_SOUNDBANK = 3094668649U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MOTION_FACTORY_BUS = 985987111U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID REVERB_ROOM = 3258416929U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID DEFAULT_MOTION_DEVICE = 4230635974U;
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
